a0a757cfa2cfe6f63ab09c2bef6cfa6faa028fa5
Adarsh Pyarelal <adarsh@arizona.edu>
2022-04-04 17:14:37 -0700

    Added clang-format, refactored into agent class

--- main.cpp
+++ main.cpp
@@ -20,27 +20,83 @@ namespace fs = boost::filesystem;
 using namespace std;
 using namespace std::chrono;
 
-void heartbeat_publisher_func(mqtt::async_client_ptr cli) {
-    while (true) {
-        this_thread::sleep_for(seconds(1));
-        cli->publish("status/agent", "ok")->wait();
-    }
+
+void process_dialog_agent_message(mqtt::const_message_ptr msg) {
+    json::value jv = json::parse(msg->to_string());
+    cout << jv << endl;
 }
 
-void subscriber_func(mqtt::async_client_ptr cli) {
-    while (true) {
-        auto msg = cli->consume_message();
+class Agent {
+    shared_ptr<mqtt::async_client> mqtt_client;
+    thread publisher, subscriber;
+    bool agent_stopped = false;
 
-        if (!msg) {
-            continue;
+  public:
+    Agent(string address) {
+        // Create an MQTT client using a smart pointer to be shared among
+        // threads.
+        this->mqtt_client = make_shared<mqtt::async_client>(address, "agent");
+
+        // Connect options for a persistent session and automatic reconnects.
+        auto connOpts = mqtt::connect_options_builder()
+                            .clean_session(true)
+                            .automatic_reconnect(seconds(2), seconds(30))
+                            .finalize();
+
+        auto topics = mqtt::string_collection::create({"agent/dialog"});
+        const vector<int> QOS{2};
+        // Start consuming _before_ connecting, because we could get a flood
+        // of stored messages as soon as the connection completes since
+        // we're using a persistent (non-clean) session with the broker.
+        this->mqtt_client->start_consuming();
+
+        BOOST_LOG_TRIVIAL(info)
+            << "Connecting to the MQTT broker at " << address << "...";
+        auto rsp = this->mqtt_client->connect(connOpts)->get_connect_response();
+    };
+
+    void start() {
+        this->publisher = thread(&Agent::heartbeat_publisher_func, this);
+        // this->subscriber = jthread(subscriber_func, this->mqtt_client);
+        // this->publisher.join();
+        // this->subscriber.join();
+    }
+
+    void disconnect() {
+        BOOST_LOG_TRIVIAL(info) << "Disconnecting...";
+        this->mqtt_client->disconnect();
+    }
+
+    void subscriber_func() {
+        while (true) {
+            mqtt::const_message_ptr msg = this->mqtt_client->consume_message();
+
+            if (!msg) {
+                continue;
+            }
+
+            string topic = msg->get_topic();
+            if (topic == "agent/dialog") {
+                process_dialog_agent_message(msg);
+            }
         }
-
-        json::value jv = json::parse(msg->to_string());
-        string topic = msg->get_topic();
-        //cout << jv << endl;
-        // cout << msg->get_topic() << ": " << msg->to_string() << endl;
     }
-}
+
+    void heartbeat_publisher_func() {
+        while (!this->agent_stopped) {
+            this_thread::sleep_for(seconds(1));
+            this->mqtt_client->publish("status/agent", "ok")->wait();
+        }
+    }
+
+    ~Agent() {
+        this->agent_stopped = true;
+        if (this->publisher.joinable()) {
+            BOOST_LOG_TRIVIAL(info) << "Shutting down publisher thread";
+            this->publisher.join();
+        }
+    }
+};
 
 int main(int argc, char* argv[]) {
 
@@ -48,10 +104,10 @@ int main(int argc, char* argv[]) {
 
     string config_path;
     generic.add_options()("help,h", "Display this help message")(
-        "version,v", "Display the version number")(
-        "config,c",
-        po::value<string>(&config_path),
-        "Path to (optional) config file.");
+        "version,v",
+        "Display the version number")("config,c",
+                                      po::value<string>(&config_path),
+                                      "Path to (optional) config file.");
 
     po::options_description config("Configuration");
 
@@ -77,8 +133,8 @@ int main(int argc, char* argv[]) {
             po::store(po::parse_config_file(config_path.c_str(), config), vm);
         }
         else {
-            BOOST_LOG_TRIVIAL(error)
-                << "Specified config file '" << config_path << "' does not exist!";
+            BOOST_LOG_TRIVIAL(error) << "Specified config file '" << config_path
+                                     << "' does not exist!";
             return EXIT_FAILURE;
         }
     }
@@ -88,54 +144,14 @@ int main(int argc, char* argv[]) {
     string address = "tcp://" + vm["mqtt.host"].as<string>() + ":" +
                      to_string(vm["mqtt.port"].as<int>());
 
-    // Create an MQTT client using a smart pointer to be shared among threads.
-    auto client = make_shared<mqtt::async_client>(address, "agent");
-
-    // Connect options for a persistent session and automatic reconnects.
-    auto connOpts = mqtt::connect_options_builder()
-                        .clean_session(false)
-                        .automatic_reconnect(seconds(2), seconds(30))
-                        .finalize();
-
-    auto topics = mqtt::string_collection::create({"test"});
-    const vector<int> QOS{2};
-
-    try {
-        // Start consuming _before_ connecting, because we could get a flood
-        // of stored messages as soon as the connection completes since
-        // we're using a persistent (non-clean) session with the broker.
-        client->start_consuming();
-
-        BOOST_LOG_TRIVIAL(info)
-            << "Connecting to the MQTT broker at " << address << "...";
-        auto rsp = client->connect(connOpts)->get_connect_response();
-        BOOST_LOG_TRIVIAL(info) << "Connected.";
-
-        // Subscribe if this is a new session with the server
-        if (!rsp.is_session_present()) {
-            client->subscribe(topics, QOS);
-        }
-
-        // Start the publisher thread
-
-        thread publisher(heartbeat_publisher_func, client);
-
-        // Consume messages in this thread
-        thread subscriber(subscriber_func, client);
-
-        publisher.join();
-        subscriber.join();
-
-        // Disconnect
-
-        BOOST_LOG_TRIVIAL(info) << "Disconnecting...";
-        client->disconnect();
-        BOOST_LOG_TRIVIAL(info) << "Disconnected";
+    Agent agent(address);
+    agent.start();
+    while(true) {
+        cout << "Agent is running" << endl;
+        this_thread::sleep_for(seconds(1));
     }
-    catch (const mqtt::exception& exc) {
-        BOOST_LOG_TRIVIAL(error) << exc.what();
-        return 1;
-    }
+
+    // Disconnect
 
     return EXIT_SUCCESS;
 }
